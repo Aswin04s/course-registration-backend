e60f4c8e4b69ba1c987b12b7bb43d1eb5ee928c2
ASWIN S <Sundarajuaswintdr@gmail.com>
2025-11-30 21:28:40 +0530

    Fix CORS configuration

--- src/main/java/com/example/Course/Registration/System/Controller/CourseController.java
+++ src/main/java/com/example/Course/Registration/System/Controller/CourseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://course-registration-frontend.vercel.app")
 public class CourseController {
 
     @Autowired
